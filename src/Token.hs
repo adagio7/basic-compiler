e81b0872db88663c1beac0838ead289b26259d3a
adagio7 <haojiepe@gmail.com>
2024-08-31 11:45:48 +0100

    feat: added lexer tokens

---
+++ src/Token.hs
@@ -0,0 +1,12 @@
+data Token
+    = TokInt Int                -- Integer literals 42
+    | TokString String          -- String literals "hello"
+    | TokKeyword String         -- Keywords like "if", "else", "while"
+    | TokIdent String           -- Identifiers like "x", "y", "z"
+    | TokOp String              -- Operators like "+", "-", "*", "/", "&&", "||"
+    | TokLParen | TokRParen    -- Parentheses
+    | TokLBrace | TokRBrace    -- Braces
+    | TokSpace
+    | TokNewline
+    | TokEOF
+    deriving (Show, Eq)
