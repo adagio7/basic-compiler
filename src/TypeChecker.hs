947fa5717abbfa945e9dfcaf2f56b4191084eee4
adagio7 <haojiepe@gmail.com>
2024-12-17 16:42:05 +0800

    Add type checker + test

---
+++ src/TypeChecker.hs
@@ -0,0 +1,109 @@
+module TypeChecker (
+    inferType
+) where
+
+import AST
+
+import qualified Data.Map as Map
+
+inferType :: TypeEnv -> Expr -> Either String Type
+inferType env expr = case expr of
+    IntLit _ -> Right TInt
+    FloatLit _ -> Right TFloat
+    Boolean _ -> Right TBool
+    StringLit _ -> Right TString
+    Null -> Right TNull
+
+    Ident str -> case Map.lookup str env of
+        Just t -> Right t
+        Nothing -> Left $ "Undefined variable: " ++ str
+
+    BinOp op e1 e2 -> do
+        t1 <- inferType env e1
+        t2 <- inferType env e2
+        case op of
+            -- Numerical Operations
+            Add -> checkNumericOp "+" t1 t2 
+            Sub -> checkNumericOp "-" t1 t2
+            Mul -> checkNumericOp "*" t1 t2
+            Div -> checkNumericOp "/" t1 t2
+            
+            -- Logical Operations
+            -- Not -> checkBoolOp "!" t1 True -- TODO: kinda hacky, change this?
+            And -> checkBoolOp "&&" t1 t2
+            Or -> checkBoolOp "||" t1 t2
+
+            -- Comparison Operations
+            Eq  -> checkComparisonOp "==" t1 t2
+            Neq -> checkComparisonOp "!=" t1 t2
+            Lt  -> checkComparisonOp "<" t1 t2
+            Gt  -> checkComparisonOp ">" t1 t2
+            Leq -> checkComparisonOp "<=" t1 t2
+            Geq -> checkComparisonOp ">=" t1 t2
+
+    -- TODO: If condition
+    -- A problem with this is that sometimes the if should return a value, sometimes it should not
+    -- If cond e1 e2 -> do
+    --     t1 <- inferType env e1
+    --     t2 <- inferType env e2
+    --     tcond <- inferType env cond
+    --     if tcond == TBool then
+    --         if t1 == t2 then
+    --             Right t1
+    --         else
+    --             Left "Type mismatch in if-else branches"
+    --     else
+    --         Left "Condition in if statement must be boolean"
+
+    Fun (Ident name) args retType body -> do
+        -- Insert the params to new env
+        -- Handles global access & variable shadowing (as new replaces old)
+        let env' = foldr(\(Ident arg, t) acc -> Map.insert arg t acc) env args
+
+        -- trace ("Function " ++ name ++ " with args " ++ show args ++ " and return type " ++ show retType)
+
+        -- Check if the body type matches the return type
+        checkReturnType env' retType body
+
+    _ -> Right TNull
+
+
+-- Checks if the type of the expression is numeric
+checkNumericOp :: String -> Type -> Type -> Either String Type
+checkNumericOp op t1 t2 
+    | t1 == TInt && t2 == TInt = Right TInt
+    | t1 == TInt && t2 == TFloat = Right TFloat
+    | t1 == TFloat && t2 == TInt = Right TFloat
+    | t1 == TFloat && t2 == TFloat = Right TFloat
+    | otherwise = Left $ "Type mismatch in numeric operation" ++ op ++ ": " ++ show t1 ++ " and " ++ show t2
+
+-- Checks if the type of the expression is boolean
+checkBoolOp :: String -> Type -> Type -> Either String Type
+checkBoolOp op t1 t2
+    | t1 == TBool && t2 == TBool = Right TBool
+    | otherwise = Left $ "Type mismatch in boolean operation" ++ op ++ ": " ++ show t1 ++ " and " ++ show t2
+    ++ show t2
+
+checkComparisonOp :: String -> Type -> Type -> Either String Type
+checkComparisonOp op t1 t2
+    | t1 == t2 = Right TBool
+    | otherwise = Left $ "Type mismatch in comparison operation" ++ op ++ ": " ++ show t1 ++ " and " ++ show t2
+    ++ show t2
+
+checkReturnType :: TypeEnv -> Type -> Expr -> Either String Type
+checkReturnType env expectedType body = case body of
+    Return e -> do
+        t <- inferType env e
+        if t == expectedType then
+            Right expectedType
+        else
+            Left "Return type does not match function return type"
+
+    -- TODO: check if branhces
+    -- Check every branch
+    -- If cond thenBranch elseBranch -> do
+    --     tthen <- checkReturnType env expectedType thenBranch
+    --     telse <- checkReturnType env expectedType elseBranch
+
+    -- No return implicilty returns null
+    _ -> Right TNull
---
+++ test/TypeCheckerSpec.hs
@@ -0,0 +1,118 @@
+module TypeCheckerSpec (
+    spec
+) where
+
+import Test.Hspec
+import Debug.Trace
+
+import AST
+import TypeChecker
+
+import qualified Data.Map as Map
+
+spec :: Spec
+spec = do
+    let typeEnv = Map.empty
+
+    describe "should type check base data types" $ do
+	it "should type check integers" $ do
+	    let result = inferType typeEnv (IntLit 123)
+	    result `shouldBe` Right TInt
+
+	it "should type check floats" $ do
+	    let result = inferType typeEnv (FloatLit 3.14)
+	    result `shouldBe` Right TFloat
+
+	it "should type check strings" $ do
+	    let result = inferType typeEnv (StringLit "hello")
+	    result `shouldBe` Right TString
+
+	it "should type check booleans" $ do
+	    let result = inferType typeEnv (Boolean True)
+	    result `shouldBe` Right TBool
+
+	it "should type check null" $ do
+	    let result = inferType typeEnv Null
+	    result `shouldBe` Right TNull
+
+    describe "should type check binary operators" $ do
+	describe "should type check numerical operations" $ do
+	    it "should type check addition" $ do
+		let result = inferType typeEnv (BinOp Add (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TInt
+
+	    it "should type check subtraction" $ do
+		let result = inferType typeEnv (BinOp Sub (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TInt
+
+	    it "should type check multiplication" $ do
+		let result = inferType typeEnv (BinOp Mul (FloatLit 1.2) (IntLit 2))
+		result `shouldBe` Right TFloat
+
+	    it "should type check division" $ do
+		let result = inferType typeEnv (BinOp Div (IntLit 1) (FloatLit 2.31))
+		result `shouldBe` Right TFloat
+	    
+	describe "should type check logical operations" $ do
+	    it "should type check and" $ do
+		let result = inferType typeEnv (BinOp And (Boolean True) (Boolean False))
+		result `shouldBe` Right TBool
+
+	    it "should type check or" $ do
+		let result = inferType typeEnv (BinOp Or (Boolean True) (Boolean False))
+		result `shouldBe` Right TBool
+
+	describe "should type check comparison operations" $ do
+	    it "should type check equality" $ do
+		let result = inferType typeEnv (BinOp Eq (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TBool
+
+	    it "should type check inequality" $ do
+		let result = inferType typeEnv (BinOp Neq (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TBool
+
+	    it "should type check less than" $ do
+		let result = inferType typeEnv (BinOp Lt (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TBool
+
+	    it "should type check greater than" $ do
+		let result = inferType typeEnv (BinOp Gt (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TBool
+
+	    it "should type check less than or equal" $ do
+		let result = inferType typeEnv (BinOp Leq (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TBool
+
+	    it "should type check greater than or equal" $ do
+		let result = inferType typeEnv (BinOp Geq (IntLit 1) (IntLit 2))
+		result `shouldBe` Right TBool
+
+    describe "should type check function" $ do
+	-- Note: Functions shouldn't add to type envrionment, as they create a new scope
+	it "should type check function with no arguments" $ do
+	    let expTypeEnv = Map.fromList []
+	    let result = inferType typeEnv (Fun (Ident "foo") [] TNull (IntLit 123))
+	    result `shouldBe` Right TNull
+	    checkEnvEquality typeEnv expTypeEnv `shouldBe` True
+
+	it "should type check function with arguments" $ do
+	    let expTypeEnv = Map.fromList []
+	    let result = inferType typeEnv (Fun (Ident "foo") [(Ident "x", TInt), (Ident "y", TFloat)] TNull (IntLit 123))
+
+	    result `shouldBe` Right TNull
+	    checkEnvEquality typeEnv expTypeEnv `shouldBe` True
+
+        it "should type check function with return type" $ do
+	    let expTypeEnv = Map.fromList []
+	    let result = inferType typeEnv (Fun (Ident "foo") [(Ident "x", TInt), (Ident "y", TFloat)] TFloat (Return (FloatLit 123)))
+	    result `shouldBe` Right TFloat
+	    checkEnvEquality typeEnv expTypeEnv `shouldBe` True
+
+
+	-- it "should type check function with return type" $ do
+
+
+checkEnvEquality :: TypeEnv -> TypeEnv -> Bool
+checkEnvEquality env1 env2 =
+    Map.toList env1 == Map.toList env2
+
